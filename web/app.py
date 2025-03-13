8a3861d6aa079942bf5abc53dadf82299fa8818f
zebra-code <news080@yeah.net>
2025-03-13 21:38:33 +0800

    app

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
-from bokeh.plotting import figure
-from bokeh.embed import components
+import bokeh.plotting as plotting
+import bokeh.embed as embed
 
 from alatavica.db import FDatabase,FTable
 from dataclasses import dataclass
@@ -23,7 +23,7 @@ app = Flask(__name__)
 class F6618HKRender:
     def __init__(self,ticker):
         self.ticker = ticker
-    def draw(self,p:figure):
+    def draw(self,p:plotting.figure):
         db = FDatabase()
         table: FTable = db.get_table(self.ticker, "1d")
 
@@ -35,24 +35,20 @@ class F6618HKRender:
             candle_width = 0.8
             candle_height = abs(data.begin_price - data.end_price)
             candle_center = (data.begin_price + data.end_price) / 2
-            color = ""
-            if data.begin_price > data.end_price:
-                color = "green"
-            else:
-                color = "red"
+            color = "red" if data.begin_price <= data.end_price else "green"
             p.line([i, i], [data.low_price, data.high_price],line_color = color)
-            p.rect(x=i, y=candle_center, width=0.8, height=candle_height,color = color)
+            p.rect(x=i, y=candle_center, width= candle_width, height=candle_height,color = color)
 @app.route("/")
 def index():
     # 创建 Bokeh 图表
     ticker = "6618.HK"
-    p = figure(title=ticker, x_axis_label="X 轴", y_axis_label="Y 轴", tools="pan,wheel_zoom,box_zoom,reset,hover")
+    p = plotting.figure(title=ticker, x_axis_label="X 轴", y_axis_label="Y 轴", tools="pan,wheel_zoom,box_zoom,reset,hover")
 
     render = F6618HKRender(ticker)
     render.draw(p)
 
     # 将图表转换为 HTML 组件
-    script, div = components(p)
+    script, div = embed.components(p)
 
     # 渲染模板并传递组件
     return render_template("index.html", script=script, div=div)
